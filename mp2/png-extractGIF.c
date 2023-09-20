2d87b477a01673c9db4ab55c7a59cbb1376af870
Eric Wei <chiwei2@illinois.edu>
2023-09-19 22:25:19 -0500

    MP submission

--- mp2/png-extractGIF.c
+++ mp2/png-extractGIF.c
@@ -1,37 +1,53 @@
 #include "png-extractGIF.h"
 
 int png_extractGIF(const char *png_filename, const char *gif_filename) {
-  int uiuc_count = 0;
-  PNG *png = PNG_open(png_filename, "r");
-  if (!png) { return ERROR_INVALID_FILE; } 
-  FILE *gif_file = fopen(gif_filename, "w");
-  if (!gif_filename) {
-     return ERROR_INVALID_CHUNK_DATA;
-  }
-  while (1) {
-    PNG_Chunk chunk;
-    if (PNG_read(png, &chunk) == 0) {
-      PNG_close(png);
-      return ERROR_INVALID_CHUNK_DATA;
+     // Open the PNG file for reading:
+     int count = 0;
+    PNG *png = PNG_open(png_filename, "r");
+    if (!png) { 
+        return ERROR_INVALID_FILE; 
     }
-   if(strcmp(chunk.type, "uiuc") == 0) {
-      fwrite(chunk.data, 1, chunk.len, gif_file);
-      uiuc_count++;
+  
+    FILE *gif = fopen(gif_filename, "wb");
+    if (!gif) { 
+        PNG_close(png);
+        return ERROR_INVALID_FILE; 
     }
-    // Check for the "IEND" chunk to exit:
-    if ( strcmp(chunk.type, "IEND") == 0 ) {
-      PNG_free_chunk(&chunk);
-      break;  
+  
+    // Read chunks until reaching "IEND" or an invalid chunk:
+    while (1) {
+        PNG_Chunk chunk;
+        if (PNG_read(png, &chunk) == 0) {
+            fclose(gif);
+            PNG_close(png);
+            return ERROR_INVALID_CHUNK_DATA;
+        }
+
+        // If this is the "uiuc" chunk, write its data to the GIF file:
+        if (strcmp(chunk.type, "uiuc") == 0) {
+            if (fwrite(chunk.data, 1, chunk.len, gif) != chunk.len) {
+                PNG_free_chunk(&chunk);
+                fclose(gif);
+                PNG_close(png);
+                return ERROR_INVALID_CHUNK_DATA;
+            }
+            count++;
+        }
+
+        // Check for the "IEND" chunk to exit:
+        if (strcmp(chunk.type, "IEND") == 0) {
+            PNG_free_chunk(&chunk);
+            break;
+        }
+        // Free the memory associated with the chunk:
+        PNG_free_chunk(&chunk);
     }
-    // Free the memory associated with the chunk we just read:
-    PNG_free_chunk(&chunk);
-  }
-   if (uiuc_count == 0){
-     PNG_close(png);
-    fclose(gif_file);
-      return 255;
-    }
-  PNG_close(png);
-  fclose(gif_file);
-  return 0; // Change the to a zero to indicate success, when your implementaiton is complete.
+        if (count == 0) {
+                fclose(gif);
+                PNG_close(png);
+                return 255;
+        }
+    fclose(gif);
+    PNG_close(png);
+    return 0;
 }
