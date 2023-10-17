480423de92efe3027f8f0d9778382638dce66b22
Eric Wei <chiwei2@illinois.edu>
2023-10-17 18:23:28 -0500

    MP submission

--- mp5/http.c
+++ mp5/http.c
@@ -12,44 +12,43 @@
  */
 #include <string.h>
 ssize_t httprequest_parse_headers(HTTPRequest *req, char *buffer, ssize_t buffer_len) {
-    // char *current = buffer;
-    // char *end = buffer + buffer_len;
-    // char *space1 = strchr(current, ' ');
-    // req->action = strndup(current, space1 - current);
-    // current = space1 + 1;
-    // char *space2 = strchr(current, ' ');
-    // req->path = strndup(current, space2 - current);
-    // current = space2 + 1;
-    // char *newline = strchr(current, '\r');
-    // req->version = strndup(current, newline - current);
-    // current = newline + 2; 
-    // _Header *last_header = NULL;
-    // while (current < end) {
-    //     if (*current == '\r' && *(current + 1) == '\n') {
-    //         current += 2;
-    //         break;
-    //     }
-    //     char *colon = strchr(current, ':');
-    //     char *header_key = strndup(current, colon - current);
-    //     current = colon + 1;
-    //     while (*current == ' ') current++;
-    //     newline = strchr(current, '\r');
-    //     char *header_value = strndup(current, newline - current);
-    //     current = newline + 2; // skip "\r\n"
-    //     _Header *new_header = malloc(sizeof(_Header));
-    //     new_header->key = header_key;
-    //     new_header->value = header_value;
-    //     new_header->next = NULL;
-    //     if (!req->head) {
-    //         req->head = new_header;
-    //     } else {
-    //         last_header->next = new_header;
-    //     }
-    //     last_header = new_header;
-    // }
-    // req->payload = current;
-    // return current - buffer;
-    return -1;
+    char *current = buffer;
+    char *end = buffer + buffer_len;
+    char *space1 = strchr(current, ' ');
+    req->action = strndup(current, space1 - current);
+    current = space1 + 1;
+    char *space2 = strchr(current, ' ');
+    req->path = strndup(current, space2 - current);
+    current = space2 + 1;
+    char *newline = strchr(current, '\r');
+    req->version = strndup(current, newline - current);
+    current = newline + 2; 
+    _Header *last_header = NULL;
+    while (current < end) {
+        if (*current == '\r' && *(current + 1) == '\n') {
+            current += 2;
+            break;
+        }
+        char *colon = strchr(current, ':');
+        char *header_key = strndup(current, colon - current);
+        current = colon + 1;
+        while (*current == ' ') current++;
+        newline = strchr(current, '\r');
+        char *header_value = strndup(current, newline - current);
+        current = newline + 2; // skip "\r\n"
+        _Header *new_header = malloc(sizeof(_Header));
+        new_header->key = header_key;
+        new_header->value = header_value;
+        new_header->next = NULL;
+        if (!req->head) {
+            req->head = new_header;
+        } else {
+            last_header->next = new_header;
+        }
+        last_header = new_header;
+    }
+    req->payload = current;
+    return current - buffer;
 }
 
 
@@ -60,69 +59,68 @@ ssize_t httprequest_parse_headers(HTTPRequest *req, char *buffer, ssize_t buffer
  * Populate a `req` from the socket `sockfd`, returning the number of bytes read to populate `req`.
  */
 ssize_t httprequest_read(HTTPRequest *req, int sockfd) {
-    // char buffer[4096];
-    // char *temp_buffer = NULL;
-    // ssize_t bytes_read, total_bytes = 0;
-    // ssize_t temp_buffer_len = 0;
-    // while (1) {
-    //     bytes_read = read(sockfd, buffer, sizeof(buffer) - 1);
-    //     if (bytes_read <= 0) {
-    //         free(temp_buffer);
-    //         return -1;  
-    //     }
-    //     buffer[bytes_read] = '\0'; 
-    //     temp_buffer = realloc(temp_buffer, temp_buffer_len + bytes_read + 1);
-    //     memcpy(temp_buffer + temp_buffer_len, buffer, bytes_read);
-    //     temp_buffer_len += bytes_read;
-    //     temp_buffer[temp_buffer_len] = '\0';
-    //     if (strstr(temp_buffer, "\r\n\r\n")) {
-    //         break;
-    //     }
-    // }
-    // if (httprequest_parse_headers(req, temp_buffer, temp_buffer_len) < 0) {
-    //     free(temp_buffer);
-    //     return -1; 
-    // }
-    // total_bytes += temp_buffer_len;
-    // _Header *header = req->head;
-    // int content_length = -1; 
-    // while (header) {
-    //     if (strcasecmp(header->key, "Content-Length") == 0) {
-    //         content_length = atoi(header->value);
-    //         break;
-    //     }
-    //     header = header->next;
-    // }
-    // if (content_length == -1 || content_length < 0) {
-    //     req->payload = NULL; 
-    //     free(temp_buffer);
-    //     return total_bytes;
-    // }
-    // if (content_length > 0) {
-    //     char *payload_start_in_buffer = strstr(temp_buffer, "\r\n\r\n") + 4;
-    //     int payload_bytes_already_read = temp_buffer_len - (payload_start_in_buffer - temp_buffer);
-    //     int payload_bytes_to_read = content_length - payload_bytes_already_read;
-    //     while (payload_bytes_to_read > 0) {
-    //         bytes_read = read(sockfd, buffer, sizeof(buffer) - 1 < payload_bytes_to_read ? sizeof(buffer) - 1 : payload_bytes_to_read);
-    //         if (bytes_read <= 0) {
-    //             free(temp_buffer);
-    //             return -1; 
-    //         }
-    //         temp_buffer = realloc(temp_buffer, temp_buffer_len + bytes_read + 1);
-    //         memcpy(temp_buffer + temp_buffer_len, buffer, bytes_read);
-    //         temp_buffer_len += bytes_read;
-    //         temp_buffer[temp_buffer_len] = '\0';
-    //         total_bytes += bytes_read;
-    //         payload_bytes_to_read -= bytes_read;
-    //     }
-    // }
-    // if (content_length > 0) {
-    //     req->payload = temp_buffer + (strstr(temp_buffer, "\r\n\r\n") + 4 - temp_buffer);
-    // } else {
-    //     req->payload = NULL;
-    // }
-    // return total_bytes;
-    return -1;
+    char buffer[4096];
+    char *temp_buffer = NULL;
+    ssize_t bytes_read, total_bytes = 0;
+    ssize_t temp_buffer_len = 0;
+    while (1) {
+        bytes_read = read(sockfd, buffer, sizeof(buffer) - 1);
+        if (bytes_read <= 0) {
+            free(temp_buffer);
+            return -1;  
+        }
+        buffer[bytes_read] = '\0'; 
+        temp_buffer = realloc(temp_buffer, temp_buffer_len + bytes_read + 1);
+        memcpy(temp_buffer + temp_buffer_len, buffer, bytes_read);
+        temp_buffer_len += bytes_read;
+        temp_buffer[temp_buffer_len] = '\0';
+        if (strstr(temp_buffer, "\r\n\r\n")) {
+            break;
+        }
+    }
+    if (httprequest_parse_headers(req, temp_buffer, temp_buffer_len) < 0) {
+        free(temp_buffer);
+        return -1; 
+    }
+    total_bytes += temp_buffer_len;
+    _Header *header = req->head;
+    int content_length = -1; 
+    while (header) {
+        if (strcasecmp(header->key, "Content-Length") == 0) {
+            content_length = atoi(header->value);
+            break;
+        }
+        header = header->next;
+    }
+    if (content_length == -1 || content_length < 0) {
+        req->payload = NULL; 
+        free(temp_buffer);
+        return total_bytes;
+    }
+    if (content_length > 0) {
+        char *payload_start_in_buffer = strstr(temp_buffer, "\r\n\r\n") + 4;
+        int payload_bytes_already_read = temp_buffer_len - (payload_start_in_buffer - temp_buffer);
+        int payload_bytes_to_read = content_length - payload_bytes_already_read;
+        while (payload_bytes_to_read > 0) {
+            bytes_read = read(sockfd, buffer, sizeof(buffer) - 1 < payload_bytes_to_read ? sizeof(buffer) - 1 : payload_bytes_to_read);
+            if (bytes_read <= 0) {
+                free(temp_buffer);
+                return -1; 
+            }
+            temp_buffer = realloc(temp_buffer, temp_buffer_len + bytes_read + 1);
+            memcpy(temp_buffer + temp_buffer_len, buffer, bytes_read);
+            temp_buffer_len += bytes_read;
+            temp_buffer[temp_buffer_len] = '\0';
+            total_bytes += bytes_read;
+            payload_bytes_to_read -= bytes_read;
+        }
+    }
+    if (content_length > 0) {
+        req->payload = temp_buffer + (strstr(temp_buffer, "\r\n\r\n") + 4 - temp_buffer);
+    } else {
+        req->payload = NULL;
+    }
+    return total_bytes;
 }
 
 
@@ -133,8 +131,7 @@ ssize_t httprequest_read(HTTPRequest *req, int sockfd) {
  * Returns the HTTP action verb for a given `req`.
  */
 const char *httprequest_get_action(HTTPRequest *req) {
-    // return req->action;
-    return NULL;
+    return req->action;
 }
 
 
@@ -144,17 +141,16 @@ const char *httprequest_get_action(HTTPRequest *req) {
  * Returns the value of the HTTP header `key` for a given `req`.
  */
 const char *httprequest_get_header(HTTPRequest *req, const char *key) {
-    // if (!req || !key) {
-    //     return NULL;
-    // }
-    // _Header* it = req->head;
-    // while (it) {
-    //     if (!strcmp(it->key, key)) {
-    //         return it->value;
-    //     }
-    //     it = it->next;
-    // }
-    // return NULL;
+    if (!req || !key) {
+        return NULL;
+    }
+    _Header* it = req->head;
+    while (it) {
+        if (!strcmp(it->key, key)) {
+            return it->value;
+        }
+        it = it->next;
+    }
     return NULL;
 }
 
@@ -165,8 +161,7 @@ const char *httprequest_get_header(HTTPRequest *req, const char *key) {
  * Returns the requested path for a given `req`.
  */
 const char *httprequest_get_path(HTTPRequest *req) {
-    // return req->path;
-    return NULL;
+    return req->path;
 }
 
 
@@ -176,16 +171,16 @@ const char *httprequest_get_path(HTTPRequest *req) {
  * Destroys a `req`, freeing all associated memory.
  */
 void httprequest_destroy(HTTPRequest *req) {
-    // free((void*)req->action);
-    // free((void*)req->path);
-    // free((void*)req->version);
+    free((void*)req->action);
+    free((void*)req->path);
+    free((void*)req->version);
 
-    // _Header *current = req->head;
-    // while (current) {
-    //     _Header *next = current->next;
-    //     free(current->key);
-    //     free(current->value);
-    //     free(current);
-    //     current = next;
-    // }
+    _Header *current = req->head;
+    while (current) {
+        _Header *next = current->next;
+        free(current->key);
+        free(current->value);
+        free(current);
+        current = next;
+    }
 }
--- mp5/server.c
+++ mp5/server.c
@@ -1,16 +1,81 @@
 #include "http.h"
-
+#include <sys/stat.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <fcntl.h>
+void client_thread(int fd) {
+    // 1. Read the HTTP Request
+    HTTPRequest req;
+    if (httprequest_read(&req, fd) < 0) {
+        // Handle error
+        write(fd, "HTTP/1.1 400 Bad Request\r\n\r\n", 28);
+        close(fd);
+        return;
+    }
 
-void *client_thread(void *vptr) {
-  int fd = *((int *)vptr);
+    // 2. Process the Request Path
+    const char *path = req.path;
+    if (strcmp(path, "/") == 0) {
+        path = "/index.html";
+    }
 
-  return NULL;
+    // Construct the full file path
+    char filepath[1024]; // or some max path length
+    snprintf(filepath, sizeof(filepath), "static%s", path);
+
+    // 3. Check if the Requested File Exists
+    if (access(filepath, F_OK) == -1) {
+        // Send 404 Not Found response
+        write(fd, "HTTP/1.1 404 Not Found\r\n\r\n", 27);
+        close(fd);
+        return;
+    }
+
+    // 4. Determine the Content-Type
+    char *content_type = "application/octet-stream"; // default
+    if (strstr(filepath, ".html")) {
+        content_type = "text/html";
+    } else if (strstr(filepath, ".png")) {
+        content_type = "image/png";
+    }
+
+    // 5. Construct and Send the HTTP Response
+
+    // Open file for reading
+    int file_fd = open(filepath, O_RDONLY);
+    if (file_fd == -1) {
+        write(fd, "HTTP/1.1 500 Internal Server Error\r\n\r\n", 37);
+        close(fd);
+        return;
+    }
+
+    struct stat st;
+    fstat(file_fd, &st);
+    ssize_t file_size = st.st_size;
+
+    char response_header[1024];
+    int header_len = snprintf(response_header, sizeof(response_header),
+                              "HTTP/1.1 200 OK\r\n"
+                              "Content-Type: %s\r\n"
+                              "Content-Length: %zd\r\n\r\n",
+                              content_type, file_size);
+    write(fd, response_header, header_len);
+
+    // Transfer file contents
+    char buffer[1024];
+    ssize_t bytes_read;
+    while ((bytes_read = read(file_fd, buffer, sizeof(buffer))) > 0) {
+        write(fd, buffer, bytes_read);
+    }
+
+    // 6. Clean Up and Close
+    close(file_fd);
+    close(fd);
+    // You may also need to free the resources of the HTTPRequest (like headers) here if necessary
 }
 
 int main(int argc, char *argv[]) {
