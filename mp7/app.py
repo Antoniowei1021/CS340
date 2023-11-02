2b61c22bbed5b2d00a63d9c7b28d7fe39861ba57
Eric Wei <chiwei2@illinois.edu>
2023-11-01 23:25:50 -0500

    MP submission

--- mp7/app.py
+++ mp7/app.py
@@ -1,49 +1,62 @@
-from flask import Flask, render_template, send_file, request
+from flask import Flask, render_template, send_file, request, jsonify
 import os
-counter = 0    
+import subprocess
+
 app = Flask(__name__)
 
-def create_file():
-    global counter
-    filename = f"file_{counter}.gif"
-    return filename
-# Route for "/" for a web-based interface to this micro-service:
+# Keep track of the number of successfully extracted GIFs
+gif_count = 0
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
 
-# Extract a hidden "uiuc" GIF from a PNG image:
+
 @app.route('/extract', methods=["POST"])
 def extract_hidden_gif():
-  global counter
-  r = request.files['png']
-  r.save(r.filename)
-  os.system('make clean')
-  os.system('make')
-  f_ = create_file()
-  print(f"Executing: ./png-extractGIF {r.filename} {f_}")
-  result = os.system(f'./png-extractGIF {r.filename} {f_}')
-  if os.path.exists(f_):
-    print(f"File {f_} created. Size: {os.path.getsize(f_)} bytes")
-  else:
-    print(f"File {f_} was not created.")
-  result1 = os.waitstatus_to_exitcode(result)
-  print(result1)
-  if result1 == 0:
-    print(f"Sending file: {f_}")
-    counter += 1
-    return send_file(f_)
-  elif result1 == 254:
-      return "There's no hidden GIF", 415
-  else:
-      return "Invalid PNG type", 422
+    global gif_count
 
-# Get the nth saved "uiuc" GIF:
+    # Create a temporary directory if it doesn't exist
+    if not os.path.exists('temp'):
+        os.makedirs('temp')
+    
+    png_file = request.files.get('png')
+    if not png_file:
+        return "No file uploaded", 400
+
+    png_path = os.path.join('temp', f"temp_{gif_count}.png")
+    png_file.save(png_path)
+
+    gif_path = os.path.join('temp', f"extracted_{gif_count}.gif")
+    
+    # Execute the png-extractGIF program
+    exit_code = subprocess.call(["./png-extractGIF", png_path, gif_path])
+
+    # Check exit code
+    if exit_code == 0:
+        gif_count += 1
+
+        # Move the GIF to the 'gifs' directory
+        if not os.path.exists('gifs'):
+            os.makedirs('gifs')
+        os.rename(gif_path, os.path.join('gifs', f"{gif_count}.gif"))
+
+        return send_file(os.path.join('gifs', f"{gif_count}.gif"), as_attachment=True, attachment_filename=f"{gif_count}.gif")
+
+    elif exit_code == 1:
+        return "The PNG file is invalid.", 422
+    else:
+        return "The PNG does not contain a hidden GIF.", 415
+
+
 @app.route('/extract/<int:image_num>', methods=['GET'])
 def extract_image(image_num):
-    filename = f'file_{image_num}.gif'
-    if image_num <= counter:
-        return send_file(filename)
-    return "Not found", 404
-  
+    gif_path = os.path.join('gifs', f"{image_num + 1}.gif")
+    if os.path.exists(gif_path):
+        return send_file(gif_path, as_attachment=True)
+    else:
+        return "GIF not found", 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
